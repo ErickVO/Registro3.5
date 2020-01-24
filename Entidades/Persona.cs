b1615d4c21a710409c8d7fca9c9efcdf415f478a
Erick Saul Vicente Ovalle <erick_vicente@ucne.edu.do>
2020-01-24 16:10:09 -0400

    Add project files.

---
+++ Entidades/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registro.Entidades
+{
+    class Persona
+    {
+        public int PersonaID { get; set; }
+
+    }
+}
