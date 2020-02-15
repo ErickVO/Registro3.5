38ac55dcb0f5c3149a2889659243ca03fe62032a
Erick Saul Vicente Ovalle <erick_vicente@ucne.edu.do>
2020-02-15 12:03:18 -0400

    Aqui estan los Tests de la Blls

---
+++ RegistroTests/BLL/InscripcionBllTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Registro.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registro.BLL.Tests
+{
+    [TestClass()]
+    public class InscripcionBllTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AfectarBalancePTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PagoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarBalanceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ RegistroTests/BLL/PersonasBllTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Registro.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registro.BLL.Tests
+{
+    [TestClass()]
+    public class PersonasBllTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
